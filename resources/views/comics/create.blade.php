dc50184e2998651f6df4981ff04effb5bf591ed1
PietroSommaruga <pietro.sommaruga2001@gmail.com>
2022-03-09 21:42:39 +0100

    fix layout

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -9,31 +9,37 @@
         @csrf
         <div class="form-group my-3">
             <label for="exampleInputEmail1">Title</label>
-            <input type="text" class="form-control" name="title" placeholder="Enter Title" >
+            <input type="text" class="form-control my-1" name="title" placeholder="Enter Title" >
         </div>
-        <div class="form-group mb-3">
-            <label for="test">Series</label>
-            <input type="text" class="form-control" name="series" placeholder="Enter Series">
-        </div>
-        <div class="form-group mb-3">
-            <label for="test">Type</label>
-            <input type="text" class="form-control" name="type" placeholder="Enter Type">
-        </div>
-        <div class="form-group mb-3">
-            <label for="test">Sale Date</label>
-            <input type="date" class="form-control" name="sale_date">
-        </div>
-        <div class="form-group mb-3">
-            <label for="test">Price</label>
-            <input type="number" step="any" class="form-control" name="price" placeholder="Enter Price">
-        </div>
-        <div class="form-group mb-3">
-            <label for="test">Url</label>
-            <input type="url" class="form-control" name="thumb" placeholder="Enter Url">
-        </div>
+
         <div class="form-group mb-3">
             <label for="test">Description</label>
-            <textarea type="text" class="form-control" name="description" placeholder="Enter Description"></textarea>
+            <textarea type="text" class="form-control my-1" name="description" placeholder="Enter Description"></textarea>
+        </div>
+
+        <div class="form-group mb-3">
+            <label for="test">Image Url</label>
+            <input type="url" class="form-control my-1" name="thumb" placeholder="Enter Url">
+        </div>
+
+        <div class="form-group mb-3">
+            <label for="test">Price</label>
+            <input type="number" step="any" class="form-control my-1" name="price" placeholder="Enter Price">
+        </div>
+
+        <div class="form-group mb-3">
+            <label for="test">Series</label>
+            <input type="text" class="form-control my-1" name="series" placeholder="Enter Series">
+        </div>
+
+        <div class="form-group mb-3">
+            <label for="test">Select Date</label>
+            <input type="date" class="form-control my-1" name="sale_date">
+        </div>
+
+        <div class="form-group mb-3">
+            <label for="test">Type</label>
+            <input type="text" class="form-control my-1" name="type" placeholder="Enter Type">
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
