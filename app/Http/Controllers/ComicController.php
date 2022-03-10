d4af3a39704692b166d0887d65f3a3d6f9bd3051
PietroSommaruga <pietro.sommaruga2001@gmail.com>
2022-03-10 11:46:22 +0100

    fix controller

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -27,12 +27,12 @@ class ComicController extends Controller
 
 /*      $newComic = new Comic();
         $newComic->title = $comic['title'];
-        $newComic->series = $comic['series'];
-        $newComic->type = $comic['type'];
-        $newComic->sale_date = $comic['sale_date'];
-        $newComic->price = $comic['price'];
-        $newComic->thumb = $comic['thumb'];
         $newComic->description = $comic['description'];
+        $newComic->thumb = $comic['thumb'];
+        $newComic->price = $comic['price'];
+        $newComic->series = $comic['series'];
+        $newComic->sale_date = $comic['sale_date'];
+        $newComic->type = $comic['type'];
         $newComic->save();*/
 
         return redirect()->route('comics.show', $newComic->id);
